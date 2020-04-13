3537c787d00f7fb79b18b4309fdcad0a3e8475ae
daniilbelikov <pvt.belikov@gmail.com>
2020-04-13 18:51:09 +0400

    Initial commit

---
+++ PictureInAR/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PictureInAR
+//
+//  Created by Даниил Беликов on 13.04.2020.
+//  Copyright © 2020 Daniil Belikov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
